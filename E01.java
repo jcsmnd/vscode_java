c9d7e9da24595d999d1b78ffeff318806804962a
Administrator <jcsmnd@gmail.com>
2019-02-14 02:23:32 -0500

    Test git commit and push

---
+++ E01.java
@@ -0,0 +1,5 @@
+class E01{
+    public static void main(String[] args){
+        System.out.println("It's for vscode git - java");
+        }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vscode_java
-git for vscode java
+git for vscode java - Initial create date: 02/14/2019
+all rights reserved by Myungsik Kim
