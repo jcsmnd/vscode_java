05dc01b7141ed7e3dab60bc66d3935e502147ab2
Administrator <jcsmnd@gmail.com>
2019-03-11 02:12:19 -0400

    change

--- E01.java
+++ E01.java
@@ -1,5 +1,19 @@
 class E01{
     public static void main(String[] args){
-        System.out.println("It's for vscode git - java");
+        int x[] = {2,3,5};
+        int y = 2;
+        findLast(x,y);
+    }
+
+    public static int findLast(int[] x, int y){
+        for(int i=x.length-1; i>0; i--){
+            System.out.println("x["+i+"]");
+            if(x[i] == y){       
+                System.out.println("return i="+i);
+                return i;
+            }
         }
+        System.out.println("return -1");
+        return -1;
+    }
 }
