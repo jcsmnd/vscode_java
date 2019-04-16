868458f170679d54518afe007472f6f71f49ac06
Administrator <jcsmnd@gmail.com>
2019-04-16 15:20:26 -0400

    Generics

--- com/jcsmnd/Generics/Generics.java
+++ com/jcsmnd/Generics/Generics.java
@@ -3,13 +3,27 @@
 package com.jcsmnd.generics;
 
 class Generics<T>{
-    public T info;
+    private T info;
+
+    private Generics( T ref){
+        this.info = ref;
+    }
+
+    public T getInfo(){
+        return info;
+    }
+
+    public void setInfo(T info){
+        this.info = info;
+    }
 
     public static void main(String[] args){
 
-        Generics<String> person = new Generics<String>();
-
-        person.info = "Myungsik Kim";
+        Generics<String> person = new Generics<>("hi");
+        System.out.println(person.getInfo()); 
+        person.setInfo("Myungsik Kim");
+        System.out.println(person.getInfo()); 
+        
     }
 }
 
