dbb8520caa66ae63e9788da2dac0f5c96f0f4407
Administrator <jcsmnd@gmail.com>
2019-04-30 00:07:08 -0400

    Implementing BST

--- com/jcsmnd/datastructures/nonlinear/tree/Binary.java
+++ com/jcsmnd/datastructures/nonlinear/tree/Binary.java
@@ -1,5 +1,22 @@
+//written by Myungsik Kim
+//implementing BST
+
 package com.jcsmnd.datastructures.nonlinear.tree;
 
+class Node{
+    int key;
+    String value;
+    Node leftChild;
+    Node rightChild;
+
+    Node(int key, String value){
+        this.key = key;
+        this.value = value;
+    }
+}
+
 class Binary{
-    
+    public static void main(String[] args) {
+        
+    }
 }
